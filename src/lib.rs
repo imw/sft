1c4b81f97eb0ed6459d103c7d7da2d817751bdb8
Nasreen </>
2022-08-26 14:36:05 +0100

    Call dft

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,21 @@
-fn dft(inreal: &[f64], inimag: &[f64], outreal: &mut [f64], outimag: &mut [f64]) {
-	let n: usize = inreal.len();
+use rustfft::num_complex::Complex;
+
+fn dft(signal: Vec<Complex<f32>>) -> Vec<Complex<f32>> {
+	let mut output = Vec::new();
+
+	let n: usize = signal.len();
 	for k in 0 .. n {  // For each output element
-		let mut sumreal: f64 = 0.0;
-		let mut sumimag: f64 = 0.0;
+
+		let mut sumreal: f32 = 0.0;
+		let mut sumimag: f32 = 0.0;
 		for t in 0 .. n {  // For each input element
-			let angle: f64 = 2.0 * std::f64::consts::PI
-				* (t as f64) * (k as f64) / (n as f64);
-			sumreal +=  inreal[t] * angle.cos() + inimag[t] * angle.sin();
-			sumimag += -inreal[t] * angle.sin() + inimag[t] * angle.cos();
+			let angle: f32 = 2.0 * std::f32::consts::PI * (t as f32) * (k as f32) / (n as f32);
+			sumreal +=  signal[t].re * angle.cos() + signal[t].im * angle.sin();
+			sumimag += -signal[t].re * angle.sin() + signal[t].im * angle.cos();
 		}
-		outreal[k] = sumreal;
-		outimag[k] = sumimag;
+		output.push(Complex{re:sumreal, im:sumimag});
 	}
+	output
 }
 
 
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use rustfft::num_complex::Complex;
 use rustfft::algorithm::Dft;
 use rustfft::{Fft, FftDirection};
 
+
 /*
 int get_psd (double *data, int data_len, int sampling_rate, double *output_ampl, double *output_freq)
 {
@@ -40,6 +41,24 @@ fn get_psd(fft_output: &Vec<Complex<f32>>, sampling_rate: f32) -> (Vec<f32>, Vec
     return (output_amplitude, output_freq)
 }
 
+fn dft(signal: Vec<Complex<f32>>) -> Vec<Complex<f32>> {
+	let mut output = Vec::new();
+
+	let n: usize = signal.len();
+	for k in 0 .. n {  // For each output element
+
+		let mut sumreal: f32 = 0.0;
+		let mut sumimag: f32 = 0.0;
+		for t in 0 .. n {  // For each input element
+			let angle: f32 = 2.0 * std::f32::consts::PI * (t as f32) * (k as f32) / (n as f32);
+			sumreal +=  signal[t].re * angle.cos() + signal[t].im * angle.sin();
+			sumimag += -signal[t].re * angle.sin() + signal[t].im * angle.cos();
+		}
+		output.push(Complex{re:sumreal, im:sumimag});
+	}
+	output
+}
+
 fn main() {
     //TODO
     let mut signal: Vec<Complex<f32>> = Vec::new();
@@ -51,18 +70,29 @@ fn main() {
         signal.push(val);
     }
 
+    let cloned_input = signal.clone();
 
     //println!{"before process: {:?}", signal};
-    let dft = Dft::new(123, FftDirection::Forward);
-    dft.process(&mut signal);
+    let rustfft = Dft::new(123, FftDirection::Forward);
+    rustfft.process(&mut signal);
+
+    let dft_in = cloned_input.clone();
+    //dft(signal: Vec<Complex<f32>>) -> Vec<Complex<f32>>
+    let dft_out = dft(dft_in);
+
     //println!{"{:?}", signal};
-    for (k,v) in signal.iter().enumerate() {
-        println!{"{},{},{}",k,v.re*10_000_000f32,v.im*10_000_000f32};
-    }
+    // for (k,v) in signal.iter().enumerate() {
+    //     println!{"{},{},{}",k,v.re,v.im};
+    // }
 
     let (output_amplitude, output_freq) = get_psd(&signal, 60f32);
     for i in 0..output_freq.len() {
-        println!{"{},{},{}",i, output_amplitude[i], output_freq[i]};
+        println!{"{}, {}, {}",cloned_input[i], output_amplitude[i], output_freq[i]};
+    }
+
+    let (output_amplitude, output_freq) = get_psd(&dft_out, 60f32);
+    for i in 0..output_freq.len() {
+        println!{"{}, {}, {}",cloned_input[i], output_amplitude[i], output_freq[i]};
     }
 
 }
